f557f4d3e76c02127205f69cf139c5c802e96d1b
revolutionartist <joydanah17@gmail.com>
2023-06-21 20:40:11 +0300

    Replace paragraph text with this is cool

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function() {
+    // Target the paragraph element with id="text"
+    const paragraph = document.getElementById('text');
+    
+    // Replace the text content
+    paragraph.textContent = 'This is really cool!';
+  });
+  
